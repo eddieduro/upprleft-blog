704a5a0213b7b4c34d3087f793de4bd6542943d5
eddieduro <e.jamesduro@gmail.com>
2016-04-03 12:10:34 -0700

    change db url

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'stumptown-beans',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://stumptownblog.firebaseio.com/',
+    firebase: 'https://upprleftblog.firebaseio.com',
     baseURL: '/',
     locationType: 'auto',
     disqus: {
--- tmp/config_replace-output_path-ppHEPIzX.tmp/app-boot.js
+++ tmp/config_replace-output_path-ppHEPIzX.tmp/app-boot.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("stumptown-beans/app")["default"].create({"name":"stumptown-beans","version":"0.0.0+b9e812a9"});
+  require("stumptown-beans/app")["default"].create({"name":"stumptown-beans","version":"0.0.0+4f79d4a7"});
 }
 
 /* jshint ignore:end */
